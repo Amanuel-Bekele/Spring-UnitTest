732078344f591021d9e0d73f50e13b9c6f13487c
Emmanuel <amanuelbekele32@gmail.com>
2019-01-13 15:10:19 -0800

    added integration test for contextLoad

--- src/test/java/com/emmanuel/test/integration/ItemControllerIT.java
+++ src/test/java/com/emmanuel/test/integration/ItemControllerIT.java
@@ -1,10 +1,28 @@
 package com.emmanuel.test.integration;
 
+import org.json.JSONException;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest // <-- Launches up the main container
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // <-- Launches up the main container
+                                                                            // Spring will pick a port which is not in use.
 public class ItemControllerIT {
+
+    @Autowired // <-- will be aware of the port being used.
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoad() throws JSONException {
+        String expected = "[{}, {}, {}, {}, {}]";
+        String response = restTemplate.getForObject("/item", String.class);
+        JSONAssert.assertEquals(expected, response, false);
+    }
+
+
 }
